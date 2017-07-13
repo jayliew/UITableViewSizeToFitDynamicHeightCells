886a6b0d98002a3369fb33e4a8c2e6238001c79d
Jay Liew <jaysern@gmail.com>
2017-07-13 13:40:47 -0700

    A UITableView whose height "fits" (ala sizeToFit) its dynamic variable height cells, by adding up all the visible cell heights in viewDidAppear() and setting the table's height constraint constant.

--- TableViewFitContent/ViewController.swift
+++ TableViewFitContent/ViewController.swift
@@ -13,14 +13,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet var tableView: UITableView!
     @IBOutlet var tableViewHeightConstraint: NSLayoutConstraint!
     
-    @IBOutlet var parentView: UIView!
-    
-    @IBOutlet var parentViewHeightConstraint: NSLayoutConstraint!
-    
     let data = ["this is a short line.",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint. deserunt mollit anim id est laborum.",
+                "<start 1st> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint. deserunt mollit anim id est laborum. <end 1st>",
                 "another short line.",
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia."
+                "<start 2nd> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia. <end 2nd>"
                 ]
     
     override func viewDidLoad() {
@@ -33,36 +29,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.estimatedRowHeight = 50
         
     } // viewDiDload
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        var tableViewHeightShouldBe:CGFloat = 0.0
         
-        // If I add this new frame for the table's parent view,
-        // the table view height gets chopped off. See 3 lines below
-        var newFrameForParent = self.parentView.frame
-        newFrameForParent.size.height = self.tableView.contentSize.height
-        self.parentView.frame = newFrameForParent
-        // But if I comment out the above 3 lines, the table view
-        // height is fine (table height fits actual dynamic content size)
-        // but the table's parent view is shorter than the table view
+        for cell in tableView.visibleCells{
+            //print(cell.frame.height)
+            tableViewHeightShouldBe = tableViewHeightShouldBe + cell.frame.height
+        }
         
-        var newFrame = self.tableView.frame
-        newFrame.size.height = self.tableView.contentSize.height
-        self.tableView.frame = newFrame
-
-        print("viewDidAppear: tableView contentSize w: \(tableView.contentSize.width) h: \(tableView.contentSize.height)")
+        print("tableViewHeightShouldBe \(tableViewHeightShouldBe)")
+        tableViewHeightConstraint.constant = tableViewHeightShouldBe
         
-    } // viewDidAppear
-    
-    override func viewWillLayoutSubviews() {
-        super.updateViewConstraints()
-        self.tableViewHeightConstraint.constant = self.tableView.contentSize.height
-        //self.parentViewHeightConstraint.constant = self.tableView.contentSize.height + 20
-        print("viewWillLayoutSubviews: tableView contentSize w: \(tableView.contentSize.width) h: \(tableView.contentSize.height)")
-    } // viewWillLayoutSubviews
-
-    override func viewWillAppear(_ animated: Bool) {
-        //tableView.sizeToFit()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -72,9 +51,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath) as! CustomCell
         cell.cellLabel?.text = data[indexPath.row]
-        
-        print("cell \(indexPath.row) height is: \(cell.frame.height)")
-        
         return cell
     }
     
